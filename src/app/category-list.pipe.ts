0c55c4bd1d91d308e82e159f356be8bdaf371f80
MikeIsHereWorking <mbova407@yahoo.com>
2022-03-18 14:25:16 -0400

    End of Chapter 3

---
+++ src/app/category-list.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'categoryList',
+  pure: true
+})
+
+export class CategoryList implements PipeTransform {
+  transform(mediaItems: any) {
+    const categories = [];
+    mediaItems.forEach(mediaItem => {
+    if (categories.indexOf(mediaItem.category) <= -1) {
+      categories.push(mediaItem.category);
+    }
+  });
+  return categories.join(', ');
+  }
+}
